e2880d84476ce1b7662a1dcddf775fd56f798493
AValencia-Al <alexav2407@gmail.com>
2025-03-06 13:30:04 +0000

    Created the Navbar buttons, added colour, fixed position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,25 @@ import './Navbar.css'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+    const handleNavigation = (path) => {
+        window.location.href = path; // Navigate to the specified path
+    };
+
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            
+        <nav className="navbar navbar-expand-lg fixed-top">
+            <div className="container-fluid">
+                {/* Left side: Budgeting App title */}
+                <a className="navbar-brand" href="#">
+                    FinTrack
+                </a>
+
+                {/* Right side: Navigation buttons */}
+                <div className="navbar-nav ms-auto"> {/* ms-auto aligns the nav items to the right */}
+                    <button className="nav-link" onClick={() => handleNavigation('/')}>Home</button>
+                    <button className="nav-link" onClick={() => handleNavigation('/features')}>Features</button>
+                    <button className="nav-link" onClick={() => handleNavigation('/signup')}>Sign Up</button>
+                </div>
+            </div>
         </nav>
     );
 };
