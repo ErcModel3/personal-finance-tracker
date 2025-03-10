d1c3ef8d2723dffc25e5863e8a71ff9bcaf0230e
RHormann <rowanhormann@gmail.com>
2025-03-10 22:12:28 +0000

    Changed Clientjs file to hide keys from github

--- src/auth/Client.js
+++ src/auth/Client.js
@@ -1,11 +1,8 @@
-import {createClient} from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-//this needs to be put in env file and extracted, im tryna get this shit to work so i cba rn.
-const supabaseurl = "https://mkukclxfhqunsipctzto.supabase.co"
-const supabaseanonkey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rdWtjbHhmaHF1bnNpcGN0enRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExMDM5NTksImV4cCI6MjA1NjY3OTk1OX0.XqWKEXm-YYkNT-y830AjcfYuClvQcVV8CJrgS2f9Eg4";
-
-//restful endpoint to establish connection
-const supabaseClient = createClient(supabaseurl, supabaseanonkey);
+const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabaseClient;
