bf09d55151632ec4930acd8081e7f31daa130d32
Alex Kirk <76045215+ErcModel3@users.noreply.github.com>
2025-03-26 13:48:42 +0000

    Moved image and references as per react best practices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Navbar.css';
 import {Link} from 'react-router-dom';
-import logo from '../../public/logo-no-bg.png';
+import logo from '../../logo-no-bg.png';
 
 const Navbar = () => {
 
