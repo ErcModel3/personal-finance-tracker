ac848d3268f1127ff7cb3f8723f72c4ff36c1847
AValencia-Al <alexav2407@gmail.com>
2025-04-04 14:28:48 +0100

    Removed the emojis from the page

--- src/Features/AccountDetailsComponents/AccountDetail.jsx
+++ src/Features/AccountDetailsComponents/AccountDetail.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import styles from "./Settings.module.css";
 
-function AccountDetail({ icon, title, value, isSubdued = false }) {
+function AccountDetail({ title, value, isSubdued = false }) {
     return (
         <article className={styles.compactCard}>
-            <div className={styles.smallIconContainer}>{icon}</div>
             <div className={styles.detailContent}>
                 <h3 className={styles.detailTitle}>{title}</h3>
                 <p className={`${styles.detailValue} ${isSubdued ? styles.subdued : ""}`}>
