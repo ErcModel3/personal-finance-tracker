cbf7ecc3d17753cb0d795ca0d013f6aade75738b
AValencia-Al <alexav2407@gmail.com>
2025-04-08 21:33:49 +0100

    Removed transactions from navbar

--- src/components/AuthenticatedNavbar.jsx
+++ src/components/AuthenticatedNavbar.jsx
@@ -35,7 +35,6 @@ const AuthenticatedNavbar = () => {
                 {/* Right side: Navigation buttons for authenticated users */}
                 <div className="authenticated-navbar-nav ms-auto">
                     <Link className="auth-nav-link" to="/dashboard">Dashboard</Link>
-                    <Link className="auth-nav-link" to="/transactions">Transactions</Link>
                     <Link className="auth-nav-link" to="/add-expense">Add Expenses</Link>
                     <Link className="auth-nav-link" to="/manage-cards">Manage Cards</Link>
                     <Link className="auth-nav-link" to="/finance-targets">Targets</Link>
