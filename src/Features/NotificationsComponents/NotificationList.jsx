25d046937f5a53841f477bb5220931d2437d1805
RHormann <rowanhormann@gmail.com>
2025-04-09 14:05:18 +0100

    Wrote function to get targets from db and put as notifications

--- src/Features/NotificationsComponents/NotificationList.jsx
+++ src/Features/NotificationsComponents/NotificationList.jsx
@@ -1,33 +1,38 @@
 import * as React from "react";
 import styles from "./Notifications.module.css";
 import NotificationItem from "./NotificationItem.jsx";
+import userID from "../../auth/SessionData.js";
+import supabaseClient from "../../auth/Client.js";
 
-const notifications = [
-    {
-        title: "Notification 1",
-        subtitle: "You spent £100 on Wingstop, how greedy",
-    },
-    {
-        title: "Notification 2",
-        subtitle: "Your subscription to Netflix renewed",
-    },
-    {
-        title: "Notification 3",
-        subtitle: "Reminder: Your rent is due in 5 days",
-    },
-    {
-        title: "Notification 4",
-        subtitle: "Your subscription to Amazon Prime will renew in 5 days",
-    },
-    {
-        title: "Notification 5",
-        subtitle: "You spent £356 on Waitrose",
-    },
-    {
-        title: "Notification 6",
-        subtitle: "You spent £179 on Elden Ring Night Reign, great choice!",
-    },
-];
+
+
+// notifications will notify the user when there is an upcoming subscription or an ending Target.
+
+//select all targets where target end date is < 5 days from now.
+//Title : Target Name
+//£X outstandning, N Days remaining!
+const today = new Date();
+const fivedaysfromtoday = today.setDate(today.getDate() + 5);
+
+const getTargets = async () => {
+    const {data, error} = await supabaseClient
+        .from('Targets')
+        .select('*')
+        .eq('User_id', userID)
+        .gte('Target_date', today)
+        .lte('Target_date', fivedaysfromtoday);
+    if (error) console.log(error);
+    if (data){
+        for (let item of data){
+            notifications.push({
+                title: item.Name,
+                subtitle: `£${item.Goal - item.Current_completion} outstanding, N days Remaining!`,
+            });
+        }
+    }
+}
+
+const notifications = [];
 
 function NotificationList() {
     return (
